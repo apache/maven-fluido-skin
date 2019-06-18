f24b63686360851162f05939b30b502a204322ae
Michael Osipov <michaelo@apache.org>
2019-06-18 23:33:43 +0200

    [MSKINS-149] Align common Velocity structures with Default Skin

--- src/it/mskins-72/verify.groovy
+++ src/it/mskins-72/verify.groovy
@@ -21,4 +21,4 @@ File index = new File( basedir, "target/site/index.html" )
 int inceptionYear = 2002
 int currentYear = Calendar.getInstance().get(Calendar.YEAR)
 assert index.exists()
-assert 1 == index.getText().count( "<p class=\"pull-right\">Copyright &copy;" )
+assert 1 == index.getText().count( "<p class=\"pull-right\">Copyright &#169;" )
