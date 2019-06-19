f9cbe82ff307c61cefe0336da89eb0082086585a
minfrin <minfrin@users.noreply.github.com>
2019-06-19 23:05:43 +0100

    [MSKINS-153] Use XHTML5 tags in site.vm
    This closes #10

--- src/it/mskins-76_topbar/verify.groovy
+++ src/it/mskins-76_topbar/verify.groovy
@@ -20,11 +20,10 @@
 File index = new File( basedir, "target/site/index.html" )
 assert index.exists()
 assert 1 == index.getText().count( '''\
-<script type="text/javascript">
+<script>
     /* <![CDATA[ */
         (function() {
             var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
-            s.type = 'text/javascript';
             s.async = true;
             s.src = 'https://api.flattr.com/js/0.6/load.js?mode=auto';
             t.parentNode.insertBefore(s, t);
